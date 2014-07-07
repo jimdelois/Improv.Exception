ef2d7acb860ee3e2240a4d23802305d3b2df5ad3
jimdelois <jim@deloisweb.com>
2014-07-07 17:40:54 -0400

    Initial Commit of baseline functionality; Getting the Jenkins build going.

---
+++ src/Improv/Exception/Exception.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright (c) 2014, Jim DeLois
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author     Jim DeLois <%%PHPDOC_AUTHOR_EMAIL%%>
+ * @copyright  2014 Jim DeLois
+ * @license    http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause License
+ * @version    %%PHPDOC_VERSION%%
+ * @link       https://github.com/improvframework/Improv.Exception
+ * @filesource
+ *
+ */
+
+namespace Improv\Exception;
+
+/**
+ * The Base Exception Class to be thrown or extended
+ *
+ * The Exception class provides the ability to supply "sprintf"-style arguments
+ * directly to the constructor and know that the message will be correctly
+ * formatted.
+ *
+ * @author     Jim DeLois <%%PHPDOC_AUTHOR_EMAIL%%>
+ */
+class Exception extends \Exception {
+
+	public function __construct( ) {
+		$args = func_get_args( );
+
+		if ( ! isset( $args[ 0 ] ) ) {
+			throw new \UnexpectedValueException( 'Must supply at least one parameter to the Exception constructor.' );
+		}
+
+		$first = $args[ 0 ];
+		if ( ! is_string( $first ) ) {
+			throw new \UnexpectedValueException( sprintf( 'First argument must be "string", received "%s".' , gettype( $first ) ) );
+		}
+
+		foreach ( $args as $k => $v ) {
+			if ( is_object( $v ) ) {
+				throw new \UnexpectedValueException( sprintf( 'Cannot pass "object" as parameter to Exception constructor.' ) );
+			}
+		}
+
+
+		$message = $first;
+		if ( $args ) {
+			$message = call_user_func_array( 'sprintf' , $args );
+		}
+
+		parent::__construct( $message );
+	}
+}
+
+?>
---
+++ tests/unit/Improv/Exception/ExceptionTest.php
@@ -0,0 +1,163 @@
+<?php
+
+
+namespace Improv\Tests\Unit\Exception;
+
+use \ContextSpecification\Framework\Concern;
+use ContextSpecification\Framework\StaticConcern;
+use \Improv\Exception\Exception;
+
+
+
+
+
+class When_passing_nothing_to_the_constructor extends StaticConcern {
+
+	protected function context( ) {
+		$this->becauseWillThrowException( true );
+	}
+
+	protected function because( ) {
+		new Exception( );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_throw_exception( ) {
+//		$this->setExpectedException( '\UnexpectedValueException' );
+//		$this->releaseException( );
+		$this->captureException( );
+		$this->assertInstanceOf( '\UnexpectedValueException' , $this->exception );
+		$this->assertEquals( 'Must supply at least one parameter to the Exception constructor.' , $this->exception->getMessage( ) );
+	}
+}
+
+
+
+
+class When_first_argument_to_constructor_is_not_a_string extends StaticConcern {
+
+	protected function context( ) {
+		$this->becauseWillThrowException( true );
+	}
+
+	protected function because( ) {
+		new Exception( 24 , 'String as second param' );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_throw_exception( ) {
+		$this->captureException( );
+		$this->assertInstanceOf( '\UnexpectedValueException' , $this->exception );
+		$this->assertEquals( 'First argument must be "string", received "integer".' , $this->exception->getMessage( ) );
+	}
+}
+
+
+
+
+
+
+class When_any_argument_to_constructor_is_an_object extends StaticConcern {
+
+	protected function context( ) {
+		$this->becauseWillThrowException( true );
+	}
+
+
+	protected function because( ) {
+		new Exception( 'String as second param' , 24 , 'Another string' , new \stdClass() , 'Final string' );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_throw_exception( ) {
+		$this->captureException( );
+		$this->assertInstanceOf( '\UnexpectedValueException' , $this->exception );
+		$this->assertEquals( 'Cannot pass "object" as parameter to Exception constructor.' , $this->exception->getMessage( ) );
+	}
+}
+
+
+
+
+
+
+
+abstract class BasePositiveScenarioMessageTokenizationConcern extends Concern {
+
+	protected $arg_1 , $arg_2 , $arg_3 , $arg_4;
+	protected $result_actual , $result_expected;
+
+	protected function createSUT( ) {
+		return new Exception( $this->arg_1 , $this->arg_2 , $this->arg_3 , $this->arg_4 );
+	}
+
+	protected function because( ) {
+		$this->result_actual = $this->sut->getMessage( );
+	}
+
+	protected function assertMessageEquality( ) {
+		$this->assertEquals( $this->result_expected , $this->result_actual );
+	}
+}
+
+
+
+class When_providing_single_string_argument_and_nothing_else extends BasePositiveScenarioMessageTokenizationConcern {
+	protected function context( ) {
+		$this->arg_1 = 'This is a plain old message for an Exception';
+		$this->result_expected = $this->arg_1;
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_have_same_string_as_exceptions_message( ) {
+		$this->assertMessageEquality( );
+	}
+}
+
+
+
+
+
+class When_providing_format_string_and_one_additional_argument extends BasePositiveScenarioMessageTokenizationConcern {
+	protected function context( ) {
+		$this->arg_1 = 'This is a format string expecting an integer %d.';
+		$this->arg_2 = 24;
+		$this->result_expected = sprintf( $this->arg_1 , $this->arg_2 );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_have_correctly_tokenized_string( ) {
+		$this->assertMessageEquality( );
+	}
+}
+
+
+
+class When_providing_format_string_and_three_additional_arguments extends BasePositiveScenarioMessageTokenizationConcern {
+	protected function context( ) {
+		$this->arg_1 = 'This is a %s string with %d additional %s.';
+		$this->arg_2 = 'format';
+		$this->arg_3 = 3;
+		$this->arg_4 = 'arguments';
+
+		$this->result_expected = sprintf( $this->arg_1 , $this->arg_2 , $this->arg_3 , $this->arg_4 );
+	}
+
+	/**
+	 * @test
+	 */
+	public function should_have_correctly_tokenized_string( ) {
+		$this->assertMessageEquality( );
+	}
+}
+?> 
